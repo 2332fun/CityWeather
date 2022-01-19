a460b3f7004f45ffa8a615a900f3f75542ef314a
DCT <mirachan2332@gmail.com>
2022-01-18 18:36:06 -0600

    historyBtns

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,25 @@ var getUserInput = function () {
     return document.querySelector('#search-box').value;
 };
 
-var searchCall = function () {
-    document.querySelectorAll('.hide-banner').forEach(banner => {banner.classList.remove('hide-banner')});
-    var artistinputEl = document.querySelector('#search-box');
-    var artistinput = artistinputEl.value;
-    var searchTerm = getUserInput();
-    var url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=8cd14927b4cb4879b174cabe1c24f270`
-    document.querySelector('.icon-block').innerHTML = '';
+var searchCall = function (event) {
+    event.preventDefault();
+    searchHistoryFunction();
+    var searchInputEl = document.querySelector('#search-box');
+    var searchInput = searchInputEl.value;
     document.querySelector('#displaySearch').innerHTML = '';
 
-    fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${artistinput}&apikey=lXXeiiHp4jbagNs2QYj0n1bTm6Tr1Q2M`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?timezone=${searchInput}&exclude=hourly,daily&appid={7dbc84f7c262272b1b3286380cc36962}`)
         .then(response => response.json())
 
         .then(function (data) {
             console.log(data)
+            //FIX FOR ONE CALL API; NO TOTALELEMENTS
             if (data.page.totalElements === 0) {
                 displaySearch.textContent = 'No results found';
                 return;
             }
             for (var i = 0; i < 3; i++) {
+                //FIX TO FIT ONE CALL API INFO: TEMP, WIND, HUMIDITY, UV
                 var searchEl = document.createElement("div");
                 var searchAEl = document.createElement("a");
                 var searchH5El = document.createElement("h4");
@@ -38,6 +38,7 @@ var searchCall = function () {
                 searchEl.append(searchImgEl);
             }
         })
+
 }
 
 searchBtn.addEventListener("click", searchCall);
@@ -48,3 +49,45 @@ $("#search-box").keyup(function (event) {
     }
 });
 var displaySearch = document.querySelector('#displaySearch');
+var searchHistory = document.querySelector('.searchHistory');
+
+
+
+var searchHistoryFunction = function() {
+    document.querySelector('.searchHistory').innerHTML = '';
+    const searchHistoryBtns = ["historyBtn1", "historyBtn2", "historyBtn3", "historyBtn4", "historyBtn5", "historyBtn6", "historyBtn7"];
+    console.log(searchHistoryBtns.length);
+
+    var searchInputEl = document.querySelector('#search-box');
+
+    var historyBtn1 = document.createElement("button");
+    historyBtn1.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn1);
+
+    var historyBtn2 = document.createElement("button");
+    historyBtn2.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn2);
+
+    var historyBtn3 = document.createElement("button");
+    historyBtn3.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn3);
+
+    var historyBtn4 = document.createElement("button");
+    historyBtn4.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn4);
+
+    var historyBtn5 = document.createElement("button");
+    historyBtn5.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn5);
+
+    var historyBtn6 = document.createElement("button");
+    historyBtn6.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn6);
+
+    var historyBtn7 = document.createElement("button");
+    historyBtn7.textContent = searchInputEl.value;
+    searchHistory.append(historyBtn7);
+
+}
+
+searchHistoryFunction();
