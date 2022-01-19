dec5636a9decd87b88059f75967b7f02e428a04e
DCT <mirachan2332@gmail.com>
2022-01-18 20:50:19 -0600

    updated displaySearch elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,19 +25,25 @@ var searchCall = function (event) {
             }
             for (var i = 0; i < 3; i++) {
                 //FIX TO FIT ONE CALL API INFO: TEMP, WIND, HUMIDITY, UV
-                var searchEl = document.createElement("div");
-                var searchAEl = document.createElement("a");
-                var searchH5El = document.createElement("h4");
-                var searchImgEl = document.createElement("img");
-                searchImgEl.setAttribute("class", "AttractionImg");
-                searchAEl.setAttribute("href", data._embedded.attractions[i].url);
-                searchAEl.setAttribute("target", "_blank");
-                searchImgEl.setAttribute("src", data._embedded.attractions[i].images[0].url);
-                searchH5El.textContent = data._embedded.attractions[i].name;
-                displaySearch.append(searchAEl);
-                searchAEl.append(searchEl);
-                searchEl.append(searchH5El);
-                searchEl.append(searchImgEl);
+                var displaySearchEl = document.createElement("div");
+
+                var displaySearchAEl = document.createElement("a");
+                displaySearchAEl.setAttribute("href", data._embedded.attractions[i].url);
+                displaySearchAEl.setAttribute("target", "_blank");
+
+
+                var displaySearchH2El = document.createElement("h2");
+                displaySearchH2El.textContent = searchInputEl.value;
+
+
+                var displaySearchImgEl = document.createElement("img");
+                displaySearchImgEl.setAttribute("class", "Symbol");
+                // displaySearchImgEl.setAttribute("src", data._embedded.attractions[i].images[0].url);
+
+                displaySearch.append(displaySearchAEl);
+                displaySearchAEl.append(displaySearchEl);
+                displaySearchEl.append(displaySearchH2El);
+                displaySearchEl.append(displaySearchImgEl);
             }
         });
 
