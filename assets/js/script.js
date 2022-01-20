791ea23cbddee7825b5e53d0f5ac1e5fed2f693b
DCT <mirachan2332@gmail.com>
2022-01-20 10:46:45 -0600

    cleaned up script, added css to displaySearchH2

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,26 +13,22 @@ var searchCall = function (event) {
 
     document.querySelector('#displaySearch').innerHTML = '';
 
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?timezone=${searchInput}&exclude=hourly,daily&appid={7dbc84f7c262272b1b3286380cc36962}`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?q=${searchInput}&appid=7dbc84f7c262272b1b3286380cc36962`)
         .then(response => response.json())
 
         .then(function (data) {
-            console.log(data);
-            //FIX FOR ONE CALL API; NO TOTALELEMENTS
-            if (data.page.totalElements === 0) {
-                displaySearch.textContent = 'No results found';
-                return;
-            }
-            for (var i = 0; i < 3; i++) {
+
+            
                 //FIX TO FIT ONE CALL API INFO: TEMP, WIND, HUMIDITY, UV
                 var displaySearchEl = document.createElement("div");
 
                 var displaySearchAEl = document.createElement("a");
-                displaySearchAEl.setAttribute("href", data._embedded.attractions[i].url);
-                displaySearchAEl.setAttribute("target", "_blank");
+                // displaySearchAEl.setAttribute("href", data._embedded.attractions[i].url);
+                // displaySearchAEl.setAttribute("target", "_blank");
 
 
                 var displaySearchH2El = document.createElement("h2");
+                displaySearchH2El.setAttribute("id", "displaySearchH2");
                 displaySearchH2El.textContent = searchInputEl.value;
 
 
@@ -44,7 +40,7 @@ var searchCall = function (event) {
                 displaySearchAEl.append(displaySearchEl);
                 displaySearchEl.append(displaySearchH2El);
                 displaySearchEl.append(displaySearchImgEl);
-            }
+            
         });
 
 };
